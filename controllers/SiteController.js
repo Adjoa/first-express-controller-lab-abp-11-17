26ce9ef49556dae173765031d540f5147a0715a2
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-29 15:46:57 +0000

    automatically backed up by learn

--- controllers/SiteController.js
+++ controllers/SiteController.js
@@ -1,1 +1,13 @@
-const app = require('./app.js')
+SiteController = {};
+
+SiteController.Index = function(req, resp) {
+  resp.render("site/index")
+}
+
+SiteController.About = function(req, resp) {
+  resp.render("site/about")
+}
+
+SiteController.Contact = function(req, resp) {
+  resp.render("site/contact")
+}
