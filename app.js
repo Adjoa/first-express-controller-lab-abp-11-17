d3c3a84828f9762d3a4e8a271076cfad5eb18080
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-29 15:51:04 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const SiteController = require('SiteController')
 
 app.get("/", SiteController.Index)
 app.get("/about", SiteController.About)
-app.get("/index", SiteController.Index)
+app.get("/contact", SiteController.Contact)
 
 
 module.exports = app
